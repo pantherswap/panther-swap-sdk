61e22a38aab2bc8faeaef2b55122d2cf1b1a9622
panther-swap <admin@pantherswap.com>
2021-05-05 23:19:24 +0800

    Update tests

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -12,7 +12,7 @@ describe('constants', () => {
     // it('matches computed bytecode hash', () => {
     //   expect(COMPUTED_INIT_CODE_HASH).toEqual(INIT_CODE_HASH)
     // })
-    //TODO: Replace with real PancakePair.json
+    //TODO: Replace with real PantherPair.json
     it('matches computed bytecode hash', () => {
       expect(INIT_CODE_HASH).toEqual('0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66')
     })
